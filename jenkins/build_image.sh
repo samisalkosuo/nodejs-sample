da2c9157ed920e0c223b56ca729156eb1eeee714
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2018-11-16 12:02:14 +0200

    fix

--- jenkins/build_image.sh
+++ jenkins/build_image.sh
@@ -6,4 +6,4 @@ source build.env
 __ver=$VERSION
 __docker_image_name=${APP_NAME}:${__ver}
 docker build -t ${__docker_image_name} .
-docker tag ${__docker_image_name} ${APP_NAME}:latest'''
+docker tag ${__docker_image_name} ${APP_NAME}:latest
