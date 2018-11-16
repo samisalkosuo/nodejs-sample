f655008c5c1cf704936d65ee47ca7dfbb0adf386
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2018-11-16 11:23:09 +0200

    added build image

---
+++ jenkins/build_image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -o errexit
+
+source build.env
+__ver=$VERSION
+__docker_image_name=${APP_NAME}:${__ver}
+docker build -t ${__docker_image_name} .
+docker tag ${__docker_image_name} ${APP_NAME}:latest'''
