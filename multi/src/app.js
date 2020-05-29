1ed486b6e862d91de0b0fdbcb10189264234c006
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2020-05-29 15:16:01 +0300

    added ioredis

--- multi/src/app.js
+++ multi/src/app.js
@@ -1,59 +1,114 @@
 //------------------------------------------------------------------------------
-// node.js  application 
+// node.js sample application 
 //------------------------------------------------------------------------------
 
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
-
-// catch SIGINT and SIGTERM and exit
-// Using a single function to handle multiple signals
-function handle(signal) {
-    console.log(`Received ${signal}. Exiting...`);
-    process.exit(1)
-  }  
-//SIGINT is typically CTRL-C
-process.on('SIGINT', handle);
-//SIGTERM is sent to terminate process, for example docker stop sends SIGTERM
-process.on('SIGTERM', handle);
+var Redis = require('ioredis');
 
 
+//read env variables
 var appName = process.env.APP_NAME || "nodejs-sample";
 var serverPort = 6001;
 var DEBUG = process.env.DEBUG || false;
+//Redis env variables
+var REDIS_HOST = process.env.REDIS_HOST || fail("REDIS_HOST env variable not set.");
+var REDIS_PORT = process.env.REDIS_PORT || fail("REDIS_PORT env variable not set.");
+var REDIS_PASSWORD = process.env.REDIS_PASSWORD || fail("REDIS_PASSWORD env variable not set.");
+var REDIS_DB = process.env.REDIS_DB || 0
+
+var redis = null;
+
+function debug()
+{
+    if (DEBUG == "true")
+    {
+        for (i = 0; i < arguments.length; i++) 
+        {
+            console.log(arguments[i])
+        }
+    }
+}
+
+
+function fail(msg,code)
+{
+    console.log(msg);
+    //close redis connection
+    if (redis != null)
+    {
+        redis.quit()
+    }
+    process.exit(code)
+
+}
+
+// catch SIGINT and SIGTERM and exit
+// Using a single function to handle multiple signals
+function handleSignal(signal) {
+    fail(`Received ${signal}. Exiting...`,1);
+  }  
+//SIGINT is typically CTRL-C
+process.on('SIGINT', handleSignal);
+//SIGTERM is sent to terminate process, for example docker stop sends SIGTERM
+process.on('SIGTERM', handleSignal);
+
+
+//Connect to redis
+var redisUrl = `redis://:${REDIS_PASSWORD}@${REDIS_HOST}:${REDIS_PORT}/${REDIS_DB}`
+redis = new Redis(redisUrl);
 
 // create a new express server
 var app = express();
 
-var rootRequests = 0
-var testRequests = 0
 
 function increaseNumberOfRootRequests()
-{
-    rootRequests = rootRequests + 1
-
+{    
+    // increment redis key by one
+    redis.incr("rootRequests", function (err, result) {
+        if (err) {
+        console.error(err);
+        } else {
+            debug(`Redis key 'rootRequests' incremented. New value: ${result}`); 
+        }
+    });
 }
 
 function increaseNumberOfTestRequests()
 {
-    testRequests = testRequests + 1
+    // increment redis key by one
+    redis.incr("testRequests", function (err, result) {
+        if (err) {
+        console.error(err);
+        } else {
+            debug(`Redis key 'testRequests' incremented. New value: ${result}`); 
+        }
+    });
 
 }
 
-function getNumberOfRequests(path)
+//async function that calls redis to get value from a key
+//redis get returns promise and await in the code waits for the value
+//so this is like synchronous call
+//caller of this function needs to be also async and use await
+async function getNumberOfRequests(path)
 {
+    var value = 0
     if (path === "root")
     {
-        return rootRequests;
+        value = await redis.get("rootRequests")
     }
 
     if (path === "test")
     {
-        return testRequests;
+        value = await redis.get("testRequests")
     }
 
-    return 0
+    return value;
 }
+
+
 
 String.prototype.format = function () {
     var args = arguments;
@@ -101,12 +156,8 @@ app.get('/test', function(req, res) {
 
 });
 
-
-/*
-ICP/Prometheus custom metrics endpoint
-*/
-app.get('/metrics', function(req, res) {
-
+async function getMetricsData(res)
+{
     // metrics label naming
     // https://prometheus.io/docs/practices/naming/
 
@@ -114,20 +165,31 @@ app.get('/metrics', function(req, res) {
     //https://prometheus.io/docs/instrumenting/exposition_formats/
 
     var metric_prefix=appName.replace(/[^a-zA-Z0-9]+/g,"_");
-    var timestamp = (new Date()).getTime();    
+    var timestamp = (new Date()).getTime();
+    const testRequests = await getNumberOfRequests("test");
     var metricsData='# HELP {0}_test_requests_total Total number of HTTP requests to /test endpoint.\n\
 # TYPE {0}_test_requests_total counter\n\
 {0}_test_requests_total {1} {2}\n\n\
-'.format(metric_prefix,getNumberOfRequests("test"),timestamp);
+'.format(metric_prefix,testRequests,timestamp);
 
+    const rootRequests = await getNumberOfRequests("root");
     metricsData=metricsData+'# HELP {0}_root_requests_total Total number of HTTP requests to / endpoint.\n\
 # TYPE {0}_root_requests_total counter\n\
 {0}_root_requests_total {1} {2}\n\n\
-'.format(metric_prefix,getNumberOfRequests("root"),timestamp);
+'.format(metric_prefix,rootRequests,timestamp);
 
     res.writeHead(200, {"Content-Type": "text/plain; version=0.0.4"});
     res.write(metricsData, "utf-8");
     res.end(); 
+
+}
+
+/*
+ICP/Prometheus custom metrics endpoint
+*/
+app.get('/metrics', function(req, res) {
+
+    getMetricsData(res);
 
 });
 
