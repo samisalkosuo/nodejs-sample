9429c9c0034322bb9f30da57d9dfc14cb78edb98
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2021-03-22 11:26:43 +0200

    refactored

--- src/routes/calculatePi.js
+++ src/routes/calculatePi.js
@@ -9,6 +9,21 @@ import * as Utils from '../utils/utils.js';
 
 var router = express.Router();
 
+function calculatePiDigits(digits)
+{
+    let i = 1n;
+    //let x = 3n * (10n ** 1020n);
+    let x = 3n * (10n ** (digits + 20n));
+    let pi = x;
+    while (x > 0) {
+        x = x * i / ((i + 1n) * 4n);
+        pi += x / (i + 2n);
+        i += 2n;
+    }
+    let piDigits = pi / (10n ** 20n);
+    return piDigits;
+}
+
 router.get('/', function(req, res) {
     log(`Calculating pi...`);
     Data.setState ({ calculatePiRequests: Data.state.calculatePiRequests + 1 }) 
@@ -20,16 +35,7 @@ router.get('/', function(req, res) {
     }
     log(`Calculating ${digits} digits of pi...`);
     let startTime = Date.now();
-    let i = 1n;
-    //let x = 3n * (10n ** 1020n);
-    let x = 3n * (10n ** (digits + 20n));
-    let pi = x;
-    while (x > 0) {
-        x = x * i / ((i + 1n) * 4n);
-        pi += x / (i + 2n);
-        i += 2n;
-    }
-    let piDigits = pi / (10n ** 20n);
+    let piDigits = calculatePiDigits(digits);
     let endTime = Date.now();
     let elapsedSecs = (endTime - startTime)/1000.0;
 
