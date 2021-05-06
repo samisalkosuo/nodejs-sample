04756400bd2cf37aa60ff2234cb60924dcb39df6
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2021-05-06 12:02:55 +0300

    updated

--- src/routes/error.js
+++ src/routes/error.js
@@ -11,7 +11,7 @@ router.get('/', function (req, res) {
     const html = Utils.getPreHTML("error",`${variableThatDoesNotExist}`);
    }catch (err) {
         error(`${err}`);
-        var errorHtml = `<h1>500</h1><p>${err}</p>`;
+        var errorHtml = `<h2>500 Internal Server Error</h2><p>${err}</p>`;
         var html=Utils.getHTML("error 500",errorHtml);
         res.writeHead(500, {"Content-Type": "text/html"});
         res.write(html);
