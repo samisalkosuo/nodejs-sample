efa408f274f0960022948374ae2d5846f0a0135a
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2021-05-26 12:17:15 +0300

    added logdna endpoints

--- src/routes/logapi.js
+++ src/routes/logapi.js
@@ -1,14 +1,88 @@
 
+//these two lines needed to use require in Node.js >14
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 import express from 'express';
-import { debug, error } from '../utils/logger.js';
+import { debug, error,trace } from '../utils/logger.js';
 import { Data } from '../utils/data.js';
 import * as Utils from '../utils/utils.js';
 
 var router = express.Router();
 
+const logdna = require('@logdna/logger')
+
+const options = {
+  app: Data.state.appName
+, level: 'info' // set a default for when level is not provided in function calls
+}
+const ingestionKey = process.env.LOGDNA_INGESTION_KEY ? process.env.LOGDNA_INGESTION_KEY : "na";
+const LOGDNALOGGER = logdna.createLogger(ingestionKey, options)
+var logDNAEnabled = ingestionKey=="na" ? false: true;
+
 var lastLogRetrievedTimestamp = "";
 var totalLogRetrievals = 0;
 var logApiEnabled = process.env.LOGAPI_ENABLED ? true: false;
+
+var sendlogs_LogDNA = false;
+var sendErrors_LogDNA = false;
+
+let logMessages = [ "Customer detail found.",
+                    "Ticket was purchased.",
+                    "Ticket was canceled.",
+                    "Seat was selected.",
+                    "Multiple tickets purchased.",
+                    "Credit card validation was successful.",
+                    "Credit card validation failed.",
+                    "Train was selected.",
+                    "Airplane was selected.",
+                    "Bus was selected.",
+                    "Car was selected.",
+                    "Transportation method not selected.",
+                    "Destination was selected.",
+                    "Destination does not exist.",
+                    "Destination out of range."
+                ];
+
+let errorMessages = [ 
+                    "OutOfMemoryError",
+                    "IOException",
+                    "NullPointerException",
+                    "UnexpectedUserBehaviourException"
+                ];
+
+const randomValue = (list) => {
+    return list[Math.floor(Math.random() * list.length)];
+};
+
+function sendLogEntriesToLogDNA()
+{
+    if (sendlogs_LogDNA == true)
+    {
+        //send log entries to logDNA
+        var entry = randomValue(logMessages);
+        debug(`Sending "${entry}" to logDNA...`);
+        LOGDNALOGGER.log(entry);
+        var timeoutValue =  Utils.getRndInteger(1200,6300);
+        setTimeout(sendLogEntriesToLogDNA, timeoutValue);
+    }
+    
+}
+
+function sendErrorEntriesToLogDNA()
+{
+    if (sendErrors_LogDNA == true)
+    {
+        //send log entries to logDNA
+        var entry = randomValue(errorMessages);
+        debug(`Sending error "${entry}" to logDNA...`);
+        LOGDNALOGGER.error(entry);
+        var timeoutValue =  Utils.getRndInteger(3000,30000);
+        setTimeout(sendErrorEntriesToLogDNA, timeoutValue);
+    }
+    
+}
+
 
 function getHTML() {
     var now = new Date().toISOString();
@@ -19,16 +93,36 @@ function getHTML() {
         logEnabledHtml = "<p><b>Log API is not enabled. Enable it by setting environment variable:<br/>LOGAPI_ENABLED=true</b></p>";
     }
 
+    let logDNAHtml = "";
+    if (logDNAEnabled == false)
+    {
+        logDNAHtml = "<p><b>LogDNA is not enabled. Enable it by setting environment variable:<br/>LOGDNA_INGESTION_KEY=&lt;logdna-ingestion-key></b></p>";
+    }
+    else
+    {
+        //set logDNA info
+        logDNAHtml = `<p>
+        LogDNA is enabled.
+        <br/>
+        Sending logs to LogDNA: ${sendlogs_LogDNA}
+        <br/>
+        Sending errors to LogDNA: ${sendErrors_LogDNA}
+        </p>
+        `;
+    }
+
     var html = `
-<h2>Log API</h2>
+<h2>Logging test</h2>
 ${logEnabledHtml}
 <p>
 Log entries: ${Data.state.logEntries.length}
 <br/>
-Logs have been retrieved ${totalLogRetrievals} times.
+Logs have been retrieved via /logentries ${totalLogRetrievals} times.
 <br/>
 Last log retrieval:  ${lastLogRetrievedTimestamp}
 </p>
+${logDNAHtml}
+
 <p>
 Current time UTC: ${now}<br/>
 </p>
@@ -67,6 +161,49 @@ router.get('/logentries', function (req, res) {
     }    
 
 
+});
+
+router.get('/logdna/start', function (req, res) {
+
+    debug(`logDNAEnabled: ${logDNAEnabled}`);
+    if (logDNAEnabled == true)
+    {
+        //start sending log entries to logdna
+        sendlogs_LogDNA = true;
+        setTimeout(sendLogEntriesToLogDNA, 1);
+    }
+    res.redirect(req.baseUrl);
+});
+
+router.get('/logdna/stop', function (req, res) {
+
+    if (logDNAEnabled == true)
+    {
+        //stop sending log entries to logdna
+        sendlogs_LogDNA = false;
+    }
+    res.redirect(req.baseUrl);
+});
+
+router.get('/logdna/errors/start', function (req, res) {
+
+    if (logDNAEnabled == true)
+    {
+        //start sending log entries to logdna
+        sendErrors_LogDNA = true;
+        setTimeout(sendErrorEntriesToLogDNA, 1);
+    }
+    res.redirect(req.baseUrl);
+});
+
+router.get('/logdna/errors/stop', function (req, res) {
+
+    if (logDNAEnabled == true)
+    {
+        //stop sending log entries to logdna
+        sendErrors_LogDNA = false;
+    }
+    res.redirect(req.baseUrl);
 });
 
 export { router };
